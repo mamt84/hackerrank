cb39f1ef9c19c9cae73e7d988f85c6aab9e528c7
mmorcate <martin.morcate@vertivco.com>
2018-01-03 18:17:58 -0500

    Added clinic patient solution

---
+++ src/main/java/com/challenges/test/ClinicPatients.java
@@ -0,0 +1,140 @@
+package com.challenges.test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+ * Problem Definition: Find the most busy period of time in a medical clinic. For this
+ * problem the input will consist on an integer n describing the amount of patient that
+ * visit the clinic during the day, then n lines with 2 strings separated by space
+ * containing the arriving and leaving time of each patient, the time will be in military
+ * time and will follow the format hh:mm As output print 1 line containing the amount of
+ * patients in the most busy period of time follow by the initial time and final time of
+ * such period, all of them separated by 1 space.
+ */
+
+class Patient
+{
+    int  id;
+    Date initialTime;
+    Date finalTime;
+
+    public Patient( int id, Date initialTime, Date finalTime )
+    {
+        this.id = id;
+        this.initialTime = initialTime;
+        this.finalTime = finalTime;
+    }
+
+    /**
+     *
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     *
+     */
+    @Override
+    public String toString()
+    {
+        return "Patient [id=" + id + ", initialTime=" + initialTime + ", finalTime=" + finalTime + "]";
+    }
+
+}
+
+class Snapshot implements Comparable<Snapshot>
+{
+    Patient patient;
+    Date    time;
+    boolean arriving;
+    
+    public Snapshot( Patient patient, Date time, boolean arriving )
+    {
+        this.patient = patient;
+        this.time = time;
+        this.arriving = arriving;
+    }
+
+    @Override
+    public int compareTo( Snapshot other )
+    {
+        int timeComparison = time.compareTo( other.time );
+        int arrivingComparison = arriving == other.arriving ? 0 : arriving ? 1 : -1;
+        return timeComparison != 0 ? timeComparison : arriving ? 1 : -1;
+    }
+
+    /**
+     *
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     *
+     */
+    @Override
+    public String toString()
+    {
+        return "Snapshot [patient=" + patient + ", time=" + time + ", arriving=" + arriving + "]";
+    }
+
+}
+
+public class ClinicPatients
+{
+    public static void main( String[] args ) throws Exception
+    {
+        Scanner in = new Scanner( System.in );
+
+        int n = in.nextInt();
+        in.nextLine();
+
+        List<Snapshot> snapshots = new ArrayList<>( n );
+        DateFormat dateFormat = new SimpleDateFormat( "HH:mm" );
+        for ( int i = 0; i < n; i++ )
+        {
+            String[] components = in.nextLine().split( " " );
+            Patient patient = new Patient( i, dateFormat.parse( components[0] ), dateFormat.parse( components[1] ) );
+            snapshots.add( new Snapshot( patient, patient.initialTime, true ) );
+            snapshots.add( new Snapshot( patient, patient.finalTime, false ) );
+        }
+        in.close();
+
+        Collections.sort( snapshots );
+
+        int patientCount = 0;
+        int maxPatientCount = 0;
+        Patient firstPatient = snapshots.iterator().next().patient;
+        Date initialBusiestPeriod = firstPatient.initialTime;
+        Date finalBusiestPeriod = firstPatient.finalTime;
+        boolean busiest = false;
+        for ( Snapshot snapshot : snapshots )
+        {
+            if ( snapshot.arriving )
+            {
+                patientCount++;
+                if ( patientCount > maxPatientCount )
+                {
+                    maxPatientCount = patientCount;
+                    initialBusiestPeriod = snapshot.time;
+                    busiest = true;
+                }
+            }
+            else
+            {
+                if ( busiest )
+                {
+                    finalBusiestPeriod = snapshot.time;
+                    busiest = false;
+                }
+                patientCount--;
+            }
+        }
+
+        System.out.println( maxPatientCount + " " + dateFormat.format( initialBusiestPeriod ) + " "
+                + dateFormat.format( finalBusiestPeriod ) );
+    }
+
+}
--- src/main/java/com/challenges/test/CompanyMatches.java
+++ src/main/java/com/challenges/test/CompanyMatches.java
@@ -4,49 +4,66 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class CompanyMatches {
+public class CompanyMatches
+{
+    /*
+     * Problem Deifnition: Given an array of elements from the periodic table, and a list of
+     * companies, match the elements that fit in the company name. So like, In and LinkedIn
+     * will show linked[In]
+     */
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner( System.in );
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        scanner.nextLine();
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int m = scanner.nextInt();
-		scanner.nextLine();
+        Set<String> periodicElements = new HashSet<>( n );
+        while ( n-- > 0 )
+        {
+            periodicElements.add( scanner.nextLine() );
+        }
 
-		Set<String> periodicElements = new HashSet<>(n);
-		while (n-- > 0) {
-			periodicElements.add(scanner.nextLine());
-		}
-
-		while (m-- > 0) {
-			boolean replaced = false;
-			String company = scanner.nextLine();
-			for (String element : periodicElements) {
-				if (company.contains(element)) {
-					company = company.replaceAll(element, "[" + element + "]");
-					replaced = true;
-				}
-			}
-			if (!replaced) {
-				for (String element : periodicElements) {
-					String lowerCaseCompany = company.toLowerCase();
-					String replacement = "[" + element + "]";
-					int indexOf = lowerCaseCompany.lastIndexOf(element.toLowerCase());
-					while (indexOf > -1) {
-						if (indexOf + element.length() < company.length()) {
-							company = company.substring(0, indexOf) + replacement
-									+ company.substring(indexOf + element.length());
-						} else {
-							company = company.substring(0, indexOf) + replacement;
-						}
-						indexOf = lowerCaseCompany.lastIndexOf(element.toLowerCase(), indexOf - 1);
-						replaced = true;
-					}
-				}
-			}
-			if (replaced) {
-				System.out.println(company);
-			}
-		}
-		scanner.close();
-	}
+        while ( m-- > 0 )
+        {
+            boolean replaced = false;
+            String company = scanner.nextLine();
+            for ( String element : periodicElements )
+            {
+                if ( company.contains( element ) )
+                {
+                    company = company.replaceAll( element, "[" + element + "]" );
+                    replaced = true;
+                }
+            }
+            if ( !replaced )
+            {
+                for ( String element : periodicElements )
+                {
+                    String lowerCaseCompany = company.toLowerCase();
+                    String replacement = "[" + element + "]";
+                    int indexOf = lowerCaseCompany.lastIndexOf( element.toLowerCase() );
+                    while ( indexOf > -1 )
+                    {
+                        if ( indexOf + element.length() < company.length() )
+                        {
+                            company = company.substring( 0, indexOf ) + replacement
+                                    + company.substring( indexOf + element.length() );
+                        }
+                        else
+                        {
+                            company = company.substring( 0, indexOf ) + replacement;
+                        }
+                        indexOf = lowerCaseCompany.lastIndexOf( element.toLowerCase(), indexOf - 1 );
+                        replaced = true;
+                    }
+                }
+            }
+            if ( replaced )
+            {
+                System.out.println( company );
+            }
+        }
+        scanner.close();
+    }
 }
